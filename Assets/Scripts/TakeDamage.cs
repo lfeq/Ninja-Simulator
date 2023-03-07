cf619eb072f68acecbe5363569910767dbc4ffce
04WS-LAB202-10\Alumno <diego.frias@artek.edu.mx>
2023-03-07 12:45:48 -0600

    Avances 07 marzo

---
+++ Assets/Scripts/TakeDamage.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TakeDamage : MonoBehaviour
+{
+    public UnityEvent takeDamage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
